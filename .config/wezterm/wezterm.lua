8ff5033dd1fd1012550f2cca8c60faddc27164b1
Dan Allan <daniel.b.allan@gmail.com>
2025-12-21 09:16:53 -0500

    Just use the default terminal font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'IBM Plex Mono'
 config.adjust_window_size_when_changing_font_size = false
 
 return config
